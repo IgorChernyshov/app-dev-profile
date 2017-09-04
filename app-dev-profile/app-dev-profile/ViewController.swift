9e0f70d91829f5c35f53c778d036647dbc926da1
Frostfell <ichernyshov@bk.ru>
2017-09-04 19:55:32 +0300

    Initial project

---
+++ app-dev-profile/app-dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-dev-profile
+//
+//  Created by Igor Chernyshov on 04.09.17.
+//  Copyright © 2017 Frostfell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
