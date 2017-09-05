8a853a1b8c5c1282c9e2f0e9cc1a65d7bdccd3be
Frostfell <ichernyshov@bk.ru>
2017-09-05 23:24:28 +0300

    Completed Lesson 01

--- app-dev-profile/app-dev-profile/ViewController.swift
+++ app-dev-profile/app-dev-profile/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var devslopesLogo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        devslopesLogo.layer.cornerRadius = 10
     }
 
     override func didReceiveMemoryWarning() {
